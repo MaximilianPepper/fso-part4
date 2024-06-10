f070637831114a3a1e339e7cbbf53199e7a0e846
MaximilianPepper <164947441+MaximilianPepper@users.noreply.github.com>
2024-06-10 23:18:41 +0200

    es 4.8

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 
 const PORT = process.env.PORT;
-const password = process.env.MONGODB_PASSWORD;
-const MONGODB_URI = `mongodb+srv://fullstack:${password}@cluster0.g8xyl1a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 module.exports = {
   MONGODB_URI,
   PORT,
