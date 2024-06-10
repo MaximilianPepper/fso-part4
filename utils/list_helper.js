5a6427f166f78ccb16be7442de372b15d9069fd5
MaximilianPepper <164947441+MaximilianPepper@users.noreply.github.com>
2024-06-10 12:47:47 +0200

    es 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,17 @@ const totalLikes = (blogs) => {
   return blogs.reduce((acc, curr) => acc + curr.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  let favorite;
+  blogs.forEach((e) => {
+    if (!favorite) favorite = e;
+    if (e.likes > favorite.likes) favorite = e;
+  });
+  return favorite;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
