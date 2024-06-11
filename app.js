29439052b0e30dcf9c3a2ec69fcb30b0027efdbd
MaximilianPepper <164947441+MaximilianPepper@users.noreply.github.com>
2024-06-11 23:03:24 +0200

    es 4.18

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors"); //
 const mongoose = require("mongoose"); //
 const blogsRouter = require("./controllers/blogs");
 const userRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 mongoose.connect(config.MONGODB_URI);
 
@@ -12,5 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
 
 module.exports = app;
