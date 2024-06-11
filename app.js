eac9a4221c9637ead3443ba9fc1703a09b614fec
MaximilianPepper <164947441+MaximilianPepper@users.noreply.github.com>
2024-06-11 19:13:41 +0200

    es 4.15

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const app = express(); //
 const cors = require("cors"); //
 const mongoose = require("mongoose"); //
 const blogsRouter = require("./controllers/blogs");
+const userRouter = require("./controllers/users");
 
 mongoose.connect(config.MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", userRouter);
 
 module.exports = app;
---
+++ controllers/users.js
@@ -0,0 +1,25 @@
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
+
+usersRouter.post("/", async (request, response) => {
+  const { username, name, password } = request.body;
+
+  const saltRounds = 10;
+  console.log("Username:", username, name); // Debugging
+  console.log("Password:", password); // Debugging
+  console.log("Body:", request.body); // Debugging
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  response.status(201).json(savedUser);
+});
+
+module.exports = usersRouter;
